9ac996623f7508c09a5ae7249f6851ce82a8ba3c
Thomas Heller <thom.heller@gmail.com>
2016-06-15 11:57:01 +0200

    Create build_core.sh

---
+++ build_core.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo 0 > build_done${CIRCLE_NODE_INDEX}
+
+if [ ${CIRCLE_NODE_INDEX} == 0 ]
+then
+  echo "foo" > bar
+  for i in $(seq 1 $((CIRCLE_NODE_TOTAL))
+  do
+    ssh node${i} echo 1 > build_done${i}
+  done
+else
+  DONE=$(cat build_done${CIRCLE_NODE_INDEX})
+  while [[ $DONE != 1 ]]
+  do
+    inotifywait build_done${CIRCLE_NODE_INDEX}
+    DONE=$(cat build_done${CIRCLE_NODE_INDEX})
+  done
+fi
