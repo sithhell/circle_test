09dd7cc2796b93e91370b4c535ef453828459de3
Thomas Heller <thom.heller@gmail.com>
2016-06-15 12:33:49 +0200

    Update build_core.sh

--- build_core.sh
+++ build_core.sh
@@ -3,6 +3,8 @@
 BUILD_DONE=build_done${CIRCLE_NODE_INDEX}
 echo 0 > $BUILD_DONE
 
+ls -lah build/
+
 if [[ ${CIRCLE_NODE_INDEX} == 0 ]]
 then
   echo "foo" > build/bar
@@ -10,11 +12,13 @@ then
   do
     ssh node${i} echo 1 > build_done${i}
   done
+  echo "node 0 done."
 else
   DONE=$(cat $BUILD_DONE)
   while [ $DONE != 1 ]
   do
-    inotifywait $BUILD_DONE
+    inotifywait -t 10 $BUILD_DONE
     DONE=$(cat $BUILD_DONE)
   done
+  echo "node ${CIRCLE_NODE_INDEX} done"
 fi
