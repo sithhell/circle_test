087c49b2c97683280f87ba96f76cc1a9968fb939
Thomas Heller <thom.heller@gmail.com>
2016-06-15 12:07:43 +0200

    Update build_core.sh

--- build_core.sh
+++ build_core.sh
@@ -5,7 +5,7 @@ echo 0 > build_done${CIRCLE_NODE_INDEX}
 if [ ${CIRCLE_NODE_INDEX} == 0 ]
 then
   echo "foo" > build/bar
-  for i in $(seq 1 $((CIRCLE_NODE_TOTAL))
+  for i in $(seq 1 $((CIRCLE_NODE_TOTAL)))
   do
     ssh node${i} echo 1 > build_done${i}
   done
